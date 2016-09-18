6cedc3d41ac4942afe837b121b7f6f9448afd7e0
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2016-09-18 15:30:36 +0300

    refactor tests

--- src/test/java/ru/vaddya/converter/RadixConverterTest.java
+++ src/test/java/ru/vaddya/converter/RadixConverterTest.java
@@ -8,81 +8,81 @@ public class RadixConverterTest {
 
     @Test
     public void test2to8() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 2;
         int finalRadix = 8;
         String number = "1110011,1011";
 
-        assertEquals("163,54", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("163,54", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test2to10() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 2;
         int finalRadix = 10;
         String number = "1110010,101";
 
-        assertEquals("114,625", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("114,625", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test2to16() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 2;
         int finalRadix = 16;
         String number = "11110011,100101";
 
-        assertEquals("F3,94", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("F3,94", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test8to10() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 8;
         int finalRadix = 10;
         String number = "407,7";
 
-        assertEquals("263,875", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("263,875", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test10to2() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 10;
         int finalRadix = 2;
         String number = "114";
 
-        assertEquals("1110010", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("1110010", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test10to16() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 10;
         int finalRadix = 16;
         String number = "174,5";
 
-        assertEquals("AE,8", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("AE,8", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test16to2() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 16;
         int finalRadix = 2;
         String number = "1E,2F";
 
-        assertEquals("11110,00101111", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("11110,00101111", converter.convert(number, baseRadix, finalRadix));
     }
 
     @Test
     public void test16to10() {
-        RadixConverter translator = new RadixConverter();
+        RadixConverter converter = new RadixConverter();
         int baseRadix = 16;
         int finalRadix = 10;
         String number = "AE";
 
-        assertEquals("174", translator.convert(number, baseRadix, finalRadix));
+        assertEquals("174", converter.convert(number, baseRadix, finalRadix));
     }
 }
